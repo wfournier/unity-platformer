e43057d8414b6956879ed2267f5fdb85821898e4
WWilliam Fournier <wfournier2710@gmail.com>
2019-05-03 16:17:51 -0400

    Lessons 6 to 18
    - Platform prefabs
    - Player movement + animations
    - Camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    public float followAhead;
+    public float smoothing;
+
+    private Vector3 targetPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        targetPosition = new Vector3(target.transform.position.x, currentPosition.y, currentPosition.z);
+
+        if (target.transform.localScale.x > 0f)
+        {
+            targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+        }
+        else
+        {
+            targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private Vector2 currentVelocity;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentVelocity = myRigidbody.velocity;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetAxisRaw("Horizontal") > 0f) // RIGHT
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, currentVelocity.y);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f) // LEFT
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, currentVelocity.y);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else // NEUTRAL
+        {
+            myRigidbody.velocity = new Vector2(0f, currentVelocity.y);
+        }
+
+        if (Input.GetButton("Jump") && isGrounded) // JUMP
+        {
+            myRigidbody.velocity = new Vector2(currentVelocity.x, jumpSpeed);
+        }
+
+        myAnimator.SetFloat("Speed", Math.Abs(currentVelocity.x));
+        myAnimator.SetBool("Grounded", isGrounded);
+    }
+}
