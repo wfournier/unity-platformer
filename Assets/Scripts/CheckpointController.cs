8b39defd38e3ea7c18a5a341810b4dbf692c99ea
William Fournier <wfournier2710@gmail.com>
2019-05-03 20:18:24 -0400

    Lessons 21 to 24
    - Kill zone added at the bottom of the level
    - Checkpoints and respawning

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+
+    public Sprite flagClosed;
+    public Sprite flagOpened;
+
+    public bool active;
+
+    private SpriteRenderer spriteRenderer;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!active && other.CompareTag("Player"))
+        {
+            spriteRenderer.sprite = flagOpened;
+            active = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float waitToRespawn;
+    public PlayerController player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Respawn()
+    {
+        player.gameObject.SetActive(false);
+        player.transform.position = player.respawnPosition;
+        player.gameObject.SetActive(true);
+    }
+}
