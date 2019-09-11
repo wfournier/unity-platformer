e0f5bc3428c2f6675b0bff81130df5be16043782
William Fournier <wfournier2710@gmail.com>
2019-09-10 22:58:55 -0400

    Kill player script

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    #region Declarations --------------------------------------------------
+
+    private LevelManager levelManager;
+
+    #endregion
+
+
+    #region Private/Protected Methods -------------------------------------
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            levelManager.RespawnPlayer();
+        }
+    }
+
+    #endregion
+}
